c8ace3dc22e919d7ac048f8da36420856bc12eb3
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-06-14 10:02:22 +0200

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Luca Mock on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
